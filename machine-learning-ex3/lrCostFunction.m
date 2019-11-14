f2d6fbdad6f2a45389fb16cd69ecdef3fb8d49b3
Park Hyunjeong <anyflow@gmail.com>
2019-11-15 03:56:29 +0900

    Complete ex3

--- machine-learning-ex3/lrCostFunction.m
+++ machine-learning-ex3/lrCostFunction.m
@@ -36,14 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+theta_0_row1 = [0; theta(2:size(theta), :)];
 
+J = (-1 / m) * (y' * log(h) + (1 - y') * log(1 - h)) + (lambda / (2 * m)) * (theta_0_row1' * theta_0_row1);
 
-
-
-
-
-
+grad = ((1 / m) * (h - y)' * X)' + lambda / m * theta_0_row1;
 
 % =============================================================
 
